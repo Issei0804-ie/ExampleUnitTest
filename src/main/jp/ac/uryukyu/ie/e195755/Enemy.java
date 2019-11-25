4050907db882260ed90be51a4910acc322c81b16
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2019-11-25 15:37:11 +0900

    Step 2 hot bugs fix.

--- src/main/jp/ac/uryukyu/ie/e195755/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195755/Enemy.java
@@ -21,31 +21,6 @@ public class Enemy extends LivingThing {
     }
 
     /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return super.getDead();
-    }
-
-    public String getName(){
-        return super.getName();
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if (!super.getDead()) {
-            int damage = (int)(Math.random() * getAttack());
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", super.getName(), hero.getName(), damage);
-            hero.wounded(damage);
-        }
-    }
-
-    /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
